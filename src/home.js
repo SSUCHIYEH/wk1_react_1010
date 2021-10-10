4e99c9355ff6ac88d49138363d12474e6162ac03
tsaiyuyes7 <39110981+tsaiyuyes7@users.noreply.github.com>
2021-10-10 17:15:18 +0800

    first

---
+++ src/home.js
@@ -0,0 +1,69 @@
+function Home() {
+    return (
+        <div>
+            <div class="grid-container">
+                <div class="nav">
+                    <div class="nav_1_Home"><img src="../assets/img_Landing_icon-10-10.png" alt="" /></div>
+                    <div class="nav_2_About">About</div>
+                    <div class="nav_3_News">News</div>
+                    <div class="nav_4_Members">Member</div>
+                    <div class="nav_5_signin"><button class="signoutbtn">Signout</button></div>
+                </div>
+
+                <div class="header">
+                    <div class="header_1_title">
+                        <div class="font_size_5">DTD</div><br />
+                        <div class="letter_spacing_3">Women's<br />Volleyball</div>
+                    </div>
+                    <div class="header_2_line"> </div>
+                    <div class="header_3_img"></div>
+                </div>
+
+
+                <div class="main">
+                    <div class="main_1_RL"></div>
+                    <div class="main_1_LL"></div>
+                    <div class="main_1_title">最新消息</div>
+                    <div class="main_2_img"></div>
+                    <div class="main_2_art">
+                        <div class="big_text">全國大專院校設計盃</div>
+                        <div class="small_text">嘉義港坪風雨球場<br />與銘傳商設的拉鋸戰</div>
+                        <span class="readmore">read more</span>
+                    </div>
+                    <div class="main_3_img"></div>
+                    <div class="main_3_art">
+                        <div class="big_text">新世代的交替</div>
+                        <div class="small_text">非人類教練表示:我想吃蜂蜜</div>
+                    </div>
+                    <div class="main_4_img"></div>
+                    <div class="main_4_art">
+                        <div class="big_text">2019系際盃</div>
+                        <div class="small_text">決戰心諮!激戰3回合</div>
+                    </div>
+                    <div class="main_5_img"></div>
+                    <div class="main_5_art">
+                        <div class="big_text">開心吃隊聚</div>
+                        <div class="small_text">整場笑嗨嗨 與大四學姊的歡樂送舊</div>
+                    </div>
+                </div>
+                <div class="member">
+                    <div class="member_bgcolor"></div>
+                    <div class="member_LL"></div>
+                    <div class="member_RL"></div>
+                    <div class="member_title">member</div>
+                    <div class="member_1 Brown">outside spiker</div>
+                    <div class="member_2 Pink">libero</div>
+                    <div class="member_3 Brown">opposite</div>
+                    <div class="member_4 Pink">setter</div>
+                    <div class="member_5 Brown">middle</div>
+                </div>
+                <div class="footer">
+                    <div>©2019 DTDWV</div>
+                </div>
+            </div>
+        </div >
+    )
+}
+
+
+export default Home;
